7bdff01283718c29384ed3508cbda09645b47ba5
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2022-11-08 15:13:51 -0600

    Update Punto.h

--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -1,1 +1,26 @@
+//
+// Created by Mario on 08/11/2022.
+//
 
+#ifndef PUNTOS_PUNTO_H
+#define PUNTOS_PUNTO_H
+#include <vector>
+
+using namespace std;
+
+class Punto2D {
+public:
+    Punto2D();
+    void SetPosicion(float x, float y);
+    void Trasladar(float temp1, float temp2);
+    void RotarRespectoAlOrigen(float temp1);
+    void Escalar(float temp1, float temp2);
+    float GetX();
+    float GetY();
+
+private:
+    float a, b, c;
+};
+
+
+#endif //PUNTOS_PUNTO_H
