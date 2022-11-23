768f54f7d35a61044a1fbe80cb2fdc0064f32edd
MarioYGH <113196424+MarioYGH@users.noreply.github.com>
2022-11-23 11:17:15 -0600

    Create Grupo.cpp

---
+++ CargaElectrica/Grupo.cpp
@@ -0,0 +1,23 @@
+#include "Grupo.h"
+
+Grupo::Grupo() {
+    nombre = " ";
+    pka = 0;
+    carga = 0;
+}
+
+Grupo::Grupo(string nombreGrupo, float pkGrupo, int cargaGrupo) {
+    nombre = nombreGrupo;
+    pka = pkGrupo;
+    carga = cargaGrupo;
+}
+
+// esta función regresa el valor de la carga de cada uno de los grupos de la molecula
+int Grupo::GetCarga() {
+    return carga;
+}
+
+// esta función regresa el valor de pka de cada uno de los grupos de la molecula
+float Grupo::GetpK() {
+    return pka;
+}
